6f84522ca249fcf32639917e2e0308dfc95f31e6
Silver Bolt <silverbolt2014@gmail.com>
2014-12-08 15:59:41 -0500

    Finished chap1

---
+++ 1chap/test.js
@@ -0,0 +1,8 @@
+/*--This file was created to demonstrate that when the src attribute 
+of a script tag is used, inline JavaScript will not work with the 
+same script tag.  Only the JavaScript in the external file will run.
+
+To have inline and external JavaScript on a page you will need two 
+separate script tags.  One for each.
+*/
+console.log("inside test.js");
