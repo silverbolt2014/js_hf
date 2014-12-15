ac866181f1081d7c0462aa5e5eb84dc55d3aa7a3
Silver Bolt <silverbolt2014@gmail.com>
2014-12-14 20:12:27 -0500

    Finished chap5

---
+++ 5chap/code-magnets.js
@@ -0,0 +1,17 @@
+var dog = {
+    name: "Fido",
+    weight: 20.2,
+    age : 4,
+    breed: "mixed",
+    activity: "fetch balls"
+};
+
+var bark;
+if ( dog.weight > 20 ) {
+    bark = "WOOF WOOF";
+} else {
+    bar = "woof woof";
+}
+
+var speak = dog.name + " says " + bark + " when he wants to " + dog.activity;
+console.log(speak);
