505f09c20805a8b2689296867312d8890c33980d
Silver Bolt <silverbolt2014@gmail.com>
2014-12-18 15:24:45 -0500

    Finished chap7

---
+++ 7chap/lab_p305.js
@@ -0,0 +1,12 @@
+var toy = new Duck("quack quack");
+toy.quack();
+
+console.log(typeof toy);
+console.log(toy instanceof Duck);
+
+function Duck(sound){
+    this.sound = sound;
+    this.quack = function() { console.log(this.sound);};
+}
+
+
